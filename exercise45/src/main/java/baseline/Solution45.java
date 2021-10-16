5485d76204576661ab12e7daac07ac5549a84b8b
richard lopez <richardlopez813>
2021-10-15 22:38:10 -0400

    fourth commit

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,27 @@
+package baseline;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Solution45 {
+    public static void main(String[] args) throws FileNotFoundException {
+        //declare ArrayList, strings, and Arrays for processing
+        //scan information into ArrayList from input file
+        //loop through 2d array and replace "utilize" with "use"
+        //new scanner
+        //prompt for output file name
+        //open file with name given (or create it if it does not exist)
+        //loop through 2d array and print altered paragraph
+    }
+
+    public String[][] dataParser(ArrayList<String> names){
+        //turn arraylist into 2d array
+        //return array
+    }
+}
