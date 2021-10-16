bf02d4a748bcc69c754b6eb60112d7985e1074fc
richard lopez <richardlopez813>
2021-10-15 22:45:25 -0400

    sixth commit

---
+++ exercise44/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution44.java,5\e\5ed8bc95bcdb167ab4dfbdf85c9a8ea80c8aa4ce
---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,16 @@
+package baseline;
+
+import com.google.gson.Gson;
+
+public class Solution44 {
+    public static void main(String[] args) {
+        //create json parser
+        //create json object
+        //convert Json formatted input file into java data structure
+        //loop for question prompt
+        //prompt for product
+        //scan for product
+        //if not found loop again
+        //if found display properties
+    }
+}
