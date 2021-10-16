9aa675f885664fc4fd0fd7c322f73a251b7c5fd6
richard lopez <richardlopez813>
2021-10-15 22:39:59 -0400

    fifth commit

---
+++ exercise46/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution46.java,c\7\c7a5cf7864afec49876c30cd1330b01956787056
---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,24 @@
+package baseline;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Solution46 {
+    public static void main(String[] args) {
+        //declare Arraylists, Arrays, Strings, and Counters
+        //open file and scan info into list
+        //call dataParser method
+        //loop through 2d array, counting each instance of the words
+        //concat String of word type with corresponding number of astericks
+        //nested if statements to determine order of print
+        //print histogram
+    }
+
+    public String[][] dataParser(ArrayList<String> paragraph){
+        //turn arraylist into 2d array
+        //return array
+    }
+}
