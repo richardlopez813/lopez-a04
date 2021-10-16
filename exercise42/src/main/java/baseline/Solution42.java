b83d4c7a0478a6f37c4e381637d8acc50ae34e07
richard lopez <richardlopez813>
2021-10-16 13:59:53 -0400

    second commit

--- exercise42/src/main/java/baseline/Solution42.java
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -7,15 +7,51 @@ import java.util.*;
 
 public class Solution42 {
     public static void main(String[] args) {
-        //declare Arraylist to hold information
-        //print headers for output
-        //scan input file information into ArrayList
-        //call dataParser method
-        //print inside a loop
+        //create object of Solution class
+        Solution42 sol1 = new Solution42();
+
+        //declare arraylist for intake and 2d array for data parsing
+        ArrayList<String> nameSalary = new ArrayList<String>();
+        String[][] twoDarrayOfstring = new String[7][3];
+
+        //print header
+        System.out.printf("%-12s%-12s%-12s%n", "Last", "First", "Salary");
+        System.out.println("------------------------------");
+
+        //open input text and scan info into arraylist
+        try(Scanner input = new Scanner(Paths.get("exercise42_input.txt"))){
+            while(input.hasNext()){
+                nameSalary.add(input.nextLine());
+            }
+        }
+        //catch exceptions
+        catch(IOException | NoSuchElementException | IllegalStateException e){
+            e.printStackTrace();
+        }
+        //call dataParser and copy array returned from function to main method's array
+        System.arraycopy(sol1.dataParser(nameSalary), 0, twoDarrayOfstring,
+                0, twoDarrayOfstring.length);
+
+        //for loop to print 2d array
+        for(int counter = 0; counter < nameSalary.size();counter++) {
+            for (int counter2 = 0; counter2 < twoDarrayOfstring[counter].length; counter2++) {
+                System.out.printf("%-12s", twoDarrayOfstring[counter][counter2]);
+            }
+            System.out.println();
+        }
     }
 
-    public String[][] dataParser(ArrayList<String> names){
+    public String[][] dataParser(ArrayList<String> nameSalary){
+        //declare 2d array
+        String[][] twoDarrayOfString = new String[7][];
+
         //turn arraylist into 2d array
+        for(int counter = 0; counter < nameSalary.size();counter++){
+            String instance = nameSalary.get(counter);
+            twoDarrayOfString[counter] = instance.split(",", 3);
+        }
         //return array
+        return twoDarrayOfString;
     }
 }
+
---
+++ exercise42/src/test/java/baseline/Solution42Test.java
@@ -0,0 +1,21 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution42Test {
+    @Test
+    public void test_data_parser(){
+        Solution42 sol1 = new Solution42();
+        ArrayList<String> data = new ArrayList<String>();
+        data.add("coach,man,running");
+        data.add("balloon,red,round");
+        data.add("apple,banana,orange");
+
+        
+    }
+
+}
