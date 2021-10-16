5c3c061e7c92e97d5cdd41190817e65104e4c168
richard lopez <richardlopez813>
2021-10-15 22:21:05 -0400

    first commit

---
+++ exercise41/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution41.java,a\7\a7b7fd7319bfb2e7377a9a4572905ac0198ea746
---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,22 @@
+package baseline;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.IllegalStateException;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Solution41 {
+    public static void main(String[] args) {
+        //declare ArrayList to hold names
+        //scan info from input file into ArrayList
+        //call sort method
+        //output header
+        //loop through ArrayList and print each name alphabetically
+    }
+
+    public ArrayList<String> sort(ArrayList<String> names){
+        //call collections to sort arraylist
+        //return arraylist
+    }
+}
