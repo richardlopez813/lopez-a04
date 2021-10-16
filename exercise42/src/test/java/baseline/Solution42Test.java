b0901119c3d37dc6fbc8f99e8eb01612e5222ff9
richard lopez <richardlopez813>
2021-10-16 14:21:46 -0400

    third commit + working test

--- exercise42/src/test/java/baseline/Solution42Test.java
+++ exercise42/src/test/java/baseline/Solution42Test.java
@@ -10,12 +10,29 @@ class Solution42Test {
     @Test
     public void test_data_parser(){
         Solution42 sol1 = new Solution42();
+
         ArrayList<String> data = new ArrayList<String>();
         data.add("coach,man,running");
         data.add("balloon,red,round");
         data.add("apple,banana,orange");
 
-        
+        String[][] twoDarrayOfstring = new String[3][7];
+        String[][] expectedArray = {
+                {
+                    "coach", "man", "running"
+                }, //row1
+                {
+                    "balloon", "red", "round"
+                }, //row2
+                {
+                    "apple", "banana", "orange"
+                }
+        };
+
+        System.arraycopy(sol1.dataParser(data), 0, twoDarrayOfstring,
+                0, twoDarrayOfstring.length);
+
+        assertArrayEquals(expectedArray, twoDarrayOfstring);
     }
 
 }
