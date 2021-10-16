1d9971ff751383bc9b613fc47f230970011b3063
richard lopez <richardlopez813>
2021-10-15 22:34:57 -0400

    third commit

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,32 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution43 {
+    public static void main(String[] args) {
+        //declare variables
+        //prompt for site name
+        //scan site name
+        //prompt for author name
+        //scan for author name
+        //ask for javascript folder
+        //scan answer
+        //ask for CSS folder
+        //scan for answer
+        //set directory to current
+        //print path to website
+        //call html method
+        //if statement to create JavaScript folder is yes then create file
+        //if not continue and print folder not created
+        //if yes print file directory
+        //if statement to create css folder is yes then create file
+        //if not continue and print file directory
+        //if yes print file directory
+    }
+
+    public void htmlWriter(String authorName, String siteName){
+        //open html file
+        //create html file with title and author
+        //print html file directory
+    }
+}
