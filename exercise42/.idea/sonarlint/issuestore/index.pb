dd5ba4921b974c96eab0f39e5bf7fdf363a05e40
richard lopez <richardlopez813>
2021-10-15 22:31:38 -0400

    second commit

---
+++ exercise42/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution42.java,c\f\cf2d704d5d2f3d0af870581692ca07d745f0c0d9
---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,21 @@
+package baseline;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Solution42 {
+    public static void main(String[] args) {
+        //declare Arraylist to hold information
+        //print headers for output
+        //scan input file information into ArrayList
+        //call dataParser method
+        //print inside a loop
+    }
+
+    public String[][] dataParser(ArrayList<String> names){
+        //turn arraylist into 2d array
+        //return array
+    }
+}
