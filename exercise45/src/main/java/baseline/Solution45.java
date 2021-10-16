b8b3cc04e0e7eaf6cc564918009dd62f1f3465a4
richard lopez <richardlopez813>
2021-10-16 14:54:15 -0400

    second commit

--- exercise45/src/main/java/baseline/Solution45.java
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -18,10 +18,84 @@ public class Solution45 {
         //prompt for output file name
         //open file with name given (or create it if it does not exist)
         //loop through 2d array and print altered paragraph
+        Solution45 sol1 = new Solution45();
+
+        ArrayList<String> text = new ArrayList<String>();
+        String sentence = "";
+        String[][] arrOfstr = new String[text.size()][];
+
+
+        try(Scanner input = new Scanner(Paths.get("exercise45_input.txt"))){
+            while(input.hasNext()){
+                text.add(input.nextLine());
+            }
+        }
+        catch(IOException | NoSuchElementException | IllegalStateException e){
+            e.printStackTrace();
+        }
+        /*
+        for(int counter = 0; counter < text.size();counter++){
+            sentence = text.get(counter);
+            arrOfstr[counter] = sentence.split(" ", 12);
+        }
+        */
+        for(int counter = 0; counter < 3; counter++){
+            System.arraycopy(sol1.dataParser(text.get(counter)), 0, arrOfstr,
+                    counter, text.size());
+        }
+
+        for (int counter2 = 0; counter2 < 3; counter2++) {
+            for (int counter3 = 0; counter3 < arrOfstr[counter2].length; counter3++) {
+                System.out.printf("%s ", arrOfstr[counter2][counter3]);
+            }
+            System.out.print("\n");
+        }
+
+        for (int counter2 = 0; counter2 < arrOfstr.length; counter2++) {
+            for (int counter3 = 0; counter3 < arrOfstr[counter2].length; counter3++) {
+                if(arrOfstr[counter2][counter3].equals("\"utilize\"")){
+                    arrOfstr[counter2][counter3] = "\"use\"";
+                }
+                if(arrOfstr[counter2][counter3].equals("utilize")){
+                    arrOfstr[counter2][counter3] = "use";
+                }
+                if(arrOfstr[counter2][counter3].equals("utilizes")){
+                    arrOfstr[counter2][counter3] = "uses";
+                }
+            }
+        }
+
+        for (int counter2 = 0; counter2 < 3; counter2++) {
+            for (int counter3 = 0; counter3 < arrOfstr[counter2].length; counter3++) {
+                System.out.printf("%s ", arrOfstr[counter2][counter3]);
+            }
+            System.out.print("\n");
+        }
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter file or directory name:");
+
+
+        try(Formatter output = new Formatter(input.nextLine())) {
+            for (int counter2 = 0; counter2 < 3; counter2++) {
+                for (int counter3 = 0; counter3 < arrOfstr[counter2].length; counter3++) {
+                    output.format("%s ", arrOfstr[counter2][counter3]);
+                }
+                output.format("%n");
+            }
+        }
     }
 
-    public String[][] dataParser(ArrayList<String> names){
+    public String[][] dataParser(ArrayList<String> text){
+        //declare 2d array
+        String[][] twoDarrayOfString = new String[3][];
+
         //turn arraylist into 2d array
+        for(int counter = 0; counter < text.size();counter++){
+            String instance = text.get(counter);
+            twoDarrayOfString[counter] = instance.split(",", 3);
+        }
         //return array
+        return twoDarrayOfString;
     }
 }
