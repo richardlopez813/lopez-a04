6986a55c40f5c86d391807ca0ddab10e274e0664
richard lopez <richardlopez813>
2021-10-16 22:59:41 -0400

    second commit

--- exercise43/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,5 +0,0 @@
-
-<
-build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
-V
-&src/main/java/baseline/Solution43.java,b\4\b4347542199e6f8f4a0c2a06abf7605a025d7459
--- exercise43/src/main/java/baseline/Solution43.java
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -1,9 +1,12 @@
 package baseline;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 import java.util.Scanner;
 
 public class Solution43 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //declare variables
         //prompt for site name
         //scan site name
@@ -22,6 +25,54 @@ public class Solution43 {
         //if statement to create css folder is yes then create file
         //if not continue and print file directory
         //if yes print file directory
+        Scanner scanner = new Scanner(System.in);
+        String authorName = "";
+        String siteName = "";
+        String javaScript = "";
+        String css = "";
+
+        System.out.print("Site name: ");
+        siteName = scanner.nextLine();
+
+        System.out.print("Author: ");
+        authorName = scanner.nextLine();
+
+        System.out.print("Do you want a folder for JavaScript? ");
+        javaScript = scanner.nextLine();
+
+        System.out.print("Do you want a folder for CSS? ");
+        css = scanner.nextLine();
+
+        String currentDirectory = System.getProperty("user.dir");
+        currentDirectory += "\\website"+"\\"+siteName;
+
+        File websiteFolder = new File(currentDirectory);
+        File absolutePath = new File("C:\\Users\\EMS\\IdeaProjects\\assignment4\\exercise43");
+
+        URI path1 = websiteFolder.toURI();
+        URI path2 = absolutePath.toURI();
+
+        URI relativePath = path2.relativize(path1);
+        String path = relativePath.getPath();
+
+        System.out.printf("Created .%s\n",path);
+
+        if(javaScript.equalsIgnoreCase("y")){
+            File javaScriptFolder = new File(currentDirectory + "\\js");
+            System.out.printf("Created %s\n", javaScriptFolder.getCanonicalPath());
+        }
+        if(css.equalsIgnoreCase("y")){
+            File cssFolder = new File(currentDirectory + "\\css");
+            System.out.printf("Created %s\n", cssFolder.getCanonicalPath());
+        }
+
+        /*
+        if(websiteFolder.mkdir()){
+            System.out.print("created");
+        }
+        */
+
+
     }
 
     public void htmlWriter(String authorName, String siteName){
