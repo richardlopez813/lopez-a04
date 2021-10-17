c62aca8044efdc8880cfe392dae4c04890679ec3
richard lopez <richardlopez813>
2021-10-16 23:38:26 -0400

    second commit

--- exercise44/src/main/java/baseline/Solution44.java
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -1,9 +1,13 @@
 package baseline;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Iterator;
 
 public class Solution44 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //create json parser
         //create json object
         //convert Json formatted input file into java data structure
@@ -12,5 +16,13 @@ public class Solution44 {
         //scan for product
         //if not found loop again
         //if found display properties
+        //Gson gson = new Gson();
+        JsonParser parser = new JsonParser();
+
+        Object obj = parser.parse(new FileReader("\\data\\exercise44_input.json"));
+
+        JsonObject jsonObject = new JsonObject();
+
+        JsonArray products = (JsonArray) jsonObject.get("name");
     }
 }
